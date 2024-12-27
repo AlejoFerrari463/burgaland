25696e003b2ada244a1c565023d27d60766dee4d
AlejoFerrari463 <alejoferrari463@gmail.com>
2024-12-26 23:19:21 -0300

    cambio de imagenes y agregado de modal. ver detalles y desplegar en github/netlify

--- src/componentes/Extrass/Extrass.jsx
+++ src/componentes/Extrass/Extrass.jsx
@@ -4,19 +4,44 @@ import "./Extrass.css"
 
 const Extrass = ({id,nombre,imagen,precio}) => {
   return (
-    <div className='extras-card' >
 
-        <div className='extras-descripcion' >
+    <>
 
-            <h3 className='mb-3' >{nombre}</h3>
-            <p>{precio}</p>
+      <div className='extras-card' data-bs-toggle="modal" data-bs-target={`#Modal${id}`}>
 
+      <div className='extras-descripcion' >
+
+          <h3 className='mb-3' >{nombre}</h3>
+          <p>{precio}</p>
+
+      </div>
+
+      <img className='extras-foto ' src={imagen} alt={nombre} />
+
+
+      </div>
+    
+    
+    
+      <div className="modal fade" id={`Modal${id}`} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-titulo fs-5" id="exampleModalLabel">{nombre.toUpperCase()}</h1>
+            </div>
+            <div className="modal-body">
+            <img className='menu-foto' src={imagen} alt={nombre} />
+            <div>{precio}</div>
+            </div>
+            <div className="modal-footer">
+              <button type="button"  className="menu-boton" data-bs-dismiss="modal">Close</button>
+            </div>
+          </div>
         </div>
-
-        <img className='extras-foto ' src={imagen} alt="" />
-
-
-    </div>
+      </div>
+    
+    </>
+   
   )
 }
 
--- src/componentes/Item/Item.jsx
+++ src/componentes/Item/Item.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 import "./Item.css"
 
-const Item = ({nombre,ingredientes,cantidad,precio,imagen,tipo}) => {
+const Item = ({id,nombre,ingredientes,cantidad,precio,imagen,tipo}) => {
+  
+  
+  
   return (
-    <div className='menu-card' >
+
+    <>
+    
+    <div className='menu-card' data-bs-toggle="modal" data-bs-target={`#Modal${id}`} >
 
       <div className='menu-descripcion' >
 
@@ -48,7 +54,7 @@ const Item = ({nombre,ingredientes,cantidad,precio,imagen,tipo}) => {
         
         } 
 
-        {nombre == "sagrada familia" && 
+        {nombre == "sagrada hamburguesa" && 
         
         <h3 className='mb-3' >
           <span className="text-danger">SAGRA</span><span className="text-warning">DA HAM</span><span className="text-danger">BURGUESA</span>
@@ -65,12 +71,88 @@ const Item = ({nombre,ingredientes,cantidad,precio,imagen,tipo}) => {
         <p>{cantidad}</p>
         <div>{precio}</div>
       </div>
-      <img className='menu-foto' src={imagen} alt="" />
+      <img className='menu-foto' src={imagen} alt={nombre} />
       
-      
-       
 
+      
     </div>
+    
+
+    <div className="modal fade" id={`Modal${id}`} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+          {nombre == "maradona" && 
+        
+              <h1 className="modal-titulo fs-3" id="exampleModalLabel">
+                <span className="text-info">MA</span><span className="text-warning">RAD</span><span className="text-info">ONA</span>
+              </h1>
+        
+          }
+        {nombre == "fiesta mexicana" && 
+        
+        <h1 className="modal-titulo fs-3" id="exampleModalLabel">
+            <span className="text-success">FIES</span><span className="animate__animated animate__shakeY">TA MEX</span><span className="text-danger">ICANA</span>
+        </h1>
+        
+        }
+
+        {nombre == "italia" && 
+        
+        <h1 className="modal-titulo fs-3" id="exampleModalLabel">
+          BELLA <span className="text-success">ITALIA</span>
+        </h1>
+        
+        } 
+
+        {nombre == "reto tokyo" && 
+        
+        <h1 className="modal-titulo fs-3" id="exampleModalLabel">
+          RET<span className="text-danger">O T</span>OKYO
+        </h1>
+        
+        }   
+
+        {nombre == "samba do brasil" && 
+        
+        <h1 className="modal-titulo fs-3" id="exampleModalLabel">
+          <span className="text-success">SAMBA</span> <span className="text-warning"> DO </span><span className="text-success">BRASIL</span>
+        </h1>
+        
+        } 
+
+        {nombre == "sagrada hamburguesa" && 
+        
+        <h1 className="modal-titulo fs-3" id="exampleModalLabel">
+          <span className="text-danger">SAGRA</span><span className="text-warning">DA HAM</span><span className="text-danger">BURGUESA</span>
+        </h1>
+        
+        }
+        
+        {tipo != "internacional" && <h1 className="modal-titulo fs-3" id="exampleModalLabel">{nombre.toUpperCase()}</h1>}
+
+          </div>
+          <div className="modal-body">
+          <img className='menu-foto' src={imagen} alt={nombre} />
+          <p>{ingredientes}</p>
+          <p>{cantidad}</p>
+          <div>{precio}</div>
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="menu-boton" data-bs-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+   
+    
+    
+    </>
+  
+
+    
   )
 }
 
